83e905417bfef92b24a2e243796f99a5d9fa2adc
iboylm2727-a11y <iboylm2727@gmail.com>
2026-07-31 20:30:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
 function whatsappTeklif(){
 
-let secimler = document.querySelectorAll("#teklif select");
-let kutular = document.querySelectorAll("#teklif input");
-let mesajKutusu = document.querySelector("#teklif textarea");
+let selectler = document.querySelectorAll("#teklif select");
+let inputlar = document.querySelectorAll("#teklif input");
+let textarea = document.querySelector("#teklif textarea");
 
-let mesaj =
-"Merhaba, iş makinesi kiralama hakkında bilgi almak istiyorum.\n\n" +
-"🚜 Makine: " + secimler[0].value + "\n" +
-"⏰ Süre: " + secimler[1].value + "\n" +
-"👤 Ad Soyad: " + kutular[0].value + "\n" +
-"📞 Telefon: " + kutular[1].value + "\n" +
-"📍 İl: " + kutular[2].value + "\n" +
-"📍 Konum: " + kutular[3].value + "\n" +
-"📝 Not: " + mesajKutusu.value;
+let mesaj = 
+"Merhaba, iş makinesi kiralama hakkında bilgi almak istiyorum.%0A%0A" +
+"🚜 Makine: " + selectler[0].value + "%0A" +
+"⏰ Çalışma Süresi: " + selectler[1].value + "%0A" +
+"👤 Ad Soyad: " + inputlar[0].value + "%0A" +
+"📞 Telefon: " + inputlar[1].value + "%0A" +
+"📍 İl: " + inputlar[2].value + "%0A" +
+"📍 Konum: " + inputlar[3].value + "%0A" +
+"📝 Not: " + textarea.value;
 
-
-let telefon = "905462182726";
 
 window.open(
-"https://wa.me/" + telefon + "?text=" + encodeURIComponent(mesaj),
+"https://wa.me/905462182726?text=" + mesaj,
 "_blank"
 );
 
