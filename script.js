cf9f22168561d0e09b88e8efcf2047103f09ead1
iboylm2727-a11y <iboylm2727@gmail.com>
2026-07-31 20:22:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 function whatsappTeklif(){
 
-let makine = document.querySelector("select").value;
+let bilgiler = document.querySelectorAll("#teklif input");
+let makine = document.querySelector("#teklif select").value;
+let sure = document.querySelectorAll("#teklif select")[1].value;
+let not = document.querySelector("#teklif textarea").value;
 
-let mesaj = 
-"Merhaba, iş makinesi kiralama hakkında bilgi almak istiyorum.%0A%0A" +
-"Makine: " + makine;
+let mesaj =
+"Merhaba, iş makinesi kiralama için teklif almak istiyorum.%0A%0A" +
+"🚜 Makine: " + makine + "%0A" +
+"⏰ Çalışma Süresi: " + sure + "%0A" +
+"👤 Ad Soyad: " + bilgiler[0].value + "%0A" +
+"📞 Telefon: " + bilgiler[1].value + "%0A" +
+"📍 İl: " + bilgiler[2].value + "%0A" +
+"📍 Konum: " + bilgiler[3].value + "%0A" +
+"📝 Not: " + not;
+
 
 let telefon = "905462182726";
 
