085020225d67ef2c63e5c641b217aaba62d88400
iboylm2727-a11y <iboylm2727@gmail.com>
2026-08-01 22:16:36 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
 function toggleMenu() {
     document.getElementById("menu").classList.toggle("active");
 }
+function teklifGonder() {
+
+let ad = document.getElementById("ad").value;
+let telefon = document.getElementById("telefon").value;
+let konum = document.getElementById("konum").value;
+let makine = document.getElementById("makine").value;
+let sure = document.getElementById("sure").value;
+let mesaj = document.getElementById("mesaj").value;
+
+let whatsappMesaj =
+`🚜 *YENİ KİRALAMA TALEBİ*
+
+👤 Ad Soyad: ${ad}
+
+📞 Telefon: ${telefon}
+
+📍 Çalışma Yeri: ${konum}
+
+🚜 Makine: ${makine}
+
+⏳ Kiralama Süresi: ${sure}
+
+📝 Mesaj:
+${mesaj}`;
+
+let url = "https://wa.me/905462182726?text=" + encodeURIComponent(whatsappMesaj);
+
+window.open(url, "_blank");
+
+}
+
+function toggleMenu() {
+document.getElementById("menu").classList.toggle("active");
+}
+
+function secMakine(makineAdi) {
+document.getElementById("makine").value = makineAdi;
+document.getElementById("teklif").scrollIntoView({
+behavior: "smooth"
+});
+}
